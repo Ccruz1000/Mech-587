b250442d6724f3da83878131dc3398828e66be25
Ccruz1000 <Ccruz1000@hotmail.com>
2022-09-13 15:01:34 -0700

    Created hello world

---
+++ Code/Hello World.cpp
@@ -0,0 +1,12 @@
+// C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main function; where code execution begins
+int main()
+{
+	// Print "Hello World"
+	cout << "Hello World"
+}
