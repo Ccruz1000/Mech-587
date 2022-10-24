db406929ff509f9ad37f4eee2795d43907121be4
Ccruz1000 <Ccruz1000@hotmail.com>
2022-10-24 12:27:16 -0700

    midterm

--- Project 1/Projectplot.py
+++ Project 1/Projectplot.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+import math
 
 L2 = [6.048073324687e-04, 1.561666503124e-04, 3.966651043739e-05]
-grid = [289, 1089, 4225]
+grid = [1/16, 1/32, 1/64]
 
+slope = (math.log(L2[-1]) - math.log(L2[0])) / (math.log(grid[-1]) - math.log(grid[0]))
+print(slope)
 plt.loglog(grid, L2, label='L2 Norm Vs. Grid Size')
-plt.grid()
+plt.grid(which='both')
 plt.legend(loc='best')
 plt.title("L2 Norm Vs. Grid Size")
 plt.xlabel('Grid Size')
